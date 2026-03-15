4fd5c88a7f6f1305247a2c511c3502c580e6cc21
Sarthak Ahir <sarthakdahir@gmail.com>
2026-03-15 23:46:36 +0530

    {Arrow function} class 4

---
+++ Function/Class04_Arrow.js
@@ -0,0 +1,11 @@
+// () => {}
+
+const addTwo = (a, b) => {
+    return a + b;
+}
+
+console.log(addTwo(2, 3)); 
+
+const name = () => { return { username: 'sarthak' }; }
+
+console.log(name().username);
