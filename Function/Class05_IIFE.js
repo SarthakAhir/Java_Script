0da7b6f74798a0291f8bb7e26d35a58550d94318
Sarthak Ahir <sarthakdahir@gmail.com>
2026-03-16 00:22:04 +0530

    {IIFE using Arroe function} class

--- Function/Class05_IIFE.js
+++ Function/Class05_IIFE.js
@@ -4,6 +4,10 @@
 // IIFEs help to avoid this by creating a new scope for variables and functions, preventing them from being accessible outside of the IIFE.
 
 (function Db(){
+    //named IIFE
     console.log(`DB connected`);
 })(); // IIFE is invoked immediately after its definition
 
+( (name) => {
+    console.log(`DB connected using arrow function ${name}`);
+})('sarthak'); // IIFE using arrow function syntax
