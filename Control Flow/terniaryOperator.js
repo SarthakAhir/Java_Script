e9e0bee727e1d122f05990ef8765c9f42e9127fa
Sarthak Ahir <sarthakdahir@gmail.com>
2026-03-18 20:47:41 +0530

    Nullish Coalescing Operator and  Terniary Operator

---
+++ Control Flow/terniaryOperator.js
@@ -0,0 +1,19 @@
+// Nullish Coalescing Operator (??) : null undefined
+
+let val1 = 5 ?? 10;
+console.log(val1); 
+
+let val2 = null ?? 10;
+console.log(val2);
+
+let val3 = undefined ?? 10 ?? 20;
+console.log(val3);
+
+// Terniary Operator 
+
+// condition ? true : false
+
+const iceCreamRate = 40;
+
+iceCreamRate > 50 ? console.log("Expensive") : console.log("Affordable");
+
