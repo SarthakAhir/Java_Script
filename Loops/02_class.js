ed13f3f19bc8f78e598bf47fd7ff707e33a43dc2
Sarthak Ahir <sarthakdahir@gmail.com>
2026-03-22 18:24:15 +0530

    while loop

---
+++ Loops/02_class.js
@@ -0,0 +1,13 @@
+let index = 0;
+while(index < 5) {
+    // console.log(index);
+    index++;
+};
+
+let myArray = ["sarthak","vaishu","parth","saurabh"];
+
+let arr = 0;
+while (arr < myArray.length) {
+    console.log(myArray[arr]);
+    arr++;
+}
