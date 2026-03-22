3c8f283a85e58524512edf91bd17c3269df66f19
Sarthak Ahir <sarthakdahir@gmail.com>
2026-03-22 18:10:18 +0530

    for loop

---
+++ Loops/01_class.js
@@ -0,0 +1,15 @@
+// for 
+
+for (let i = 0; i< 10; i++) {
+  let element = i;
+  // console.log(element);
+}
+
+for(let i=1; i<=5;i++){
+  console.log(`Table of: ${i}`);
+
+  for(let j=1; j <=10; j++){
+    console.log(`${i} * ${j} = ${i * j}`);
+  }
+  
+}
