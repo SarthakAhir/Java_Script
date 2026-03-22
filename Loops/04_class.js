fe095905d4f0944e66e0da3a2e9006ed453c12ed
Sarthak Ahir <sarthakdahir@gmail.com>
2026-03-22 18:53:24 +0530

    for of

---
+++ Loops/04_class.js
@@ -0,0 +1,13 @@
+// for of
+
+const arr = [1, 2, 3, 4, 5];
+
+for (let num of arr) {
+    console.log(num);
+}
+
+let greeting = "Hello, World!";
+
+for ( let char of greeting){
+    console.log(`Each character: ${char}`);
+}
