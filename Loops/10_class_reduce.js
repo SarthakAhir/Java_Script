8f2655e5d847e68a423a0f87bc621b4df0e69780
Sarthak Ahir <sarthakdahir@gmail.com>
2026-03-27 14:07:36 +0530

    example of reduce function

--- Loops/10_class_reduce.js
+++ Loops/10_class_reduce.js
@@ -1,7 +1,30 @@
 const myNums = [1,2,3]
 
 const myTotal = myNums.reduce((acc, currval) => {
-    console.log(`acc: ${acc}, currval: ${currval}`);
+    // console.log(`acc: ${acc}, currval: ${currval}`);
     return acc + currval;
 },0);
-console.log(myTotal);
+// console.log(myTotal);
+
+const shoppingCart = [
+    {
+        item: "js course",
+        price:3000
+    },
+    {
+        item: "css course",
+        price:2000
+    },
+    {
+        item: "html course",
+        price:1000
+    },
+    {
+        item: "react course",
+        price:3999
+    }
+]
+
+let priceToPay = shoppingCart.reduce((acc, course) => acc + course.price, 0);
+
+console.log(priceToPay);
